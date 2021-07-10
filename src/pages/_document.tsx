127eddef17d139291b578aadc92e20233fd0489f
Joe Blau <im@joeblau.com>
2021-07-10 10:51:24 -0700

    Add styled document

---
+++ src/pages/_document.tsx
@@ -0,0 +1,23 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import React from "react";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: any) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en" translate="no">
+        <Head />
+        <body className="bg-white dark:bg-black">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
