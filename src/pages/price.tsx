3fe14376a54826b49be0085f90053bace80de16c
Joe Blau <im@joeblau.com>
2021-07-10 18:05:16 -0700

    Add inline chart

--- src/pages/price.tsx
+++ src/pages/price.tsx
@@ -1,18 +1,17 @@
 import SidebarNavMenu from "../components/ui/SidebarNavMenu";
-import TradingViewWidget, { Themes } from "react-tradingview-widget";
 import { useDarkMode } from "next-dark-mode";
+import { AdvancedRealTimeChart } from "react-ts-tradingview-widgets";
 
 const Price = () => {
   const { darkModeActive } = useDarkMode();
 
   return (
     <SidebarNavMenu>
-      <main className="flex-1 relative z-0 overflow-y-auto focus:outline-none h-screen w-screen">
-        <TradingViewWidget
-          symbol="HEXUSDC"
-          theme={darkModeActive ? Themes.DARK : Themes.LIGHT}
-          locale="en"
-          autosize
+      <main className="flex-1 relative z-0 overflow-y-auto focus:outline-none">
+        <AdvancedRealTimeChart
+          autosize={true}
+          symbol="UNISWAP:HEXUSDC"
+          theme={darkModeActive ? "dark" : "light"}
         />
       </main>
     </SidebarNavMenu>
