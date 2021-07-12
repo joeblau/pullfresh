cec73ffe1c9d810fb5fbf078e1408cf4a439e3fe
Joe Blau <im@joeblau.com>
2021-07-11 22:29:28 -0700

    Extra menu into it's own class

--- src/components/SiteLayout.tsx
+++ src/components/SiteLayout.tsx
@@ -1,110 +1,13 @@
 import { Fragment, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
-import { useRouter } from "next/router";
-import Link from "next/link";
 import Image from "next/image";
 import Head from "next/head";
-import {
-  HomeIcon,
-  MenuIcon,
-  PresentationChartLineIcon,
-  TrendingUpIcon,
-  ViewListIcon,
-  XIcon,
-} from "@heroicons/react/outline";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
 import AddPublicKey from "./ui/AddPublicKeys";
-
-const account = [
-  { name: "Dashboard", href: "/", icon: HomeIcon },
-  {
-    name: "Stakes",
-    href: "/stakes",
-    icon: ViewListIcon,
-    count: "19",
-  },
-];
-
-const charts = [
-  { name: "Price", href: "/price", icon: TrendingUpIcon },
-  {
-    name: "Comparison",
-    href: "/comparison",
-    icon: PresentationChartLineIcon,
-  },
-];
-
-function classNames(...classes: any) {
-  return classes.filter(Boolean).join(" ");
-}
+import NavMenu from "./ui/NavMenu";
 
 const SiteLayout = (props: any) => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
-
-  const router = useRouter();
-
-  const NavMenuItem = (props: any) => (
-    // <Link href={props.item.href}>
-    <a
-      href={props.item.href}
-      className={classNames(
-        props.item.href == router.pathname
-          ? "bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-gray-200"
-          : "text-gray-600 dark:text-gray-300",
-        "hover:bg-gray-50 dark:hover:bg-gray-900",
-        "hover:text-gray-900 dark:hover:text-gray-100",
-        "group flex items-center px-2 py-2 text-sm font-medium rounded-md"
-      )}
-    >
-      <props.item.icon
-        className={classNames(
-          props.item.href == router.pathname
-            ? "text-gray-500 dark:text-gray-300"
-            : "text-gray-400 dark:text-gray-400",
-          "group-hover:text-gray-500",
-          "mr-3 flex-shrink-0 h-6 w-6"
-        )}
-        aria-hidden="true"
-      />
-      <span className="truncate">{props.item.name}</span>
-      {props.item.count ? (
-        <span
-          className={classNames(
-            props.item.href == router.pathname
-              ? "bg-white dark:bg-black"
-              : "bg-gray-100 dark:bg-gray-800",
-            "group-hover:bg-gray-200 dark:group-hover:bg-gray-700",
-            "ml-auto inline-block py-0.5 px-3 text-xs rounded-full"
-          )}
-        >
-          {props.item.count}
-        </span>
-      ) : null}
-    </a>
-    // </Link>
-  );
-
-  const NavMenu = () => (
-    <nav className="mt-5 flex-1 px-2 bg-white dark:bg-black space-y-1">
-      <div className="space-y-1">
-        {account.map((item) => (
-          <NavMenuItem key={item.name} item={item} />
-        ))}
-      </div>
-      <div className="pt-8">
-        <h3
-          className="px-3 text-xs font-semibold text-gray-500 dark:text-gray-300 uppercase tracking-wider"
-          id="projects-headline"
-        >
-          Charts
-        </h3>
-        <div className="mt-1 space-y-1" aria-labelledby="projects-headline">
-          {charts.map((item) => (
-            <NavMenuItem key={item.name} item={item} />
-          ))}
-        </div>
-      </div>
-    </nav>
-  );
 
   return (
     <div className="h-screen flex overflow-hidden bg-white dark:bg-black">
