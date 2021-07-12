de81c3407527f9be1b8bfbc2657cc0d7e6036325
Joe Blau <im@joeblau.com>
2021-07-11 23:27:19 -0700

    Test replace

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,7 +17,7 @@ class MyDocument extends Document {
             name="apple-mobile-web-app-status-bar-style"
             content="default"
           />
-          <meta name="apple-mobile-web-app-title" content="PWA App" />
+          <meta name="apple-mobile-web-app-title" content="Pullfresh" />
           <meta name="description" content="Pullfresh" />
           <meta name="format-detection" content="telephone=no" />
           <meta name="mobile-web-app-capable" content="yes" />
